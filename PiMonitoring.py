bf1475c9226b69f47896a76da3bf15e8f39cc458
Timothy Lock <lock.k.timothy@gmail.com>
2016-06-20 19:06:21 -0400

    Quick Init Script + disp cleanup

--- PiMonitoring.py
+++ PiMonitoring.py
@@ -1,4 +1,33 @@
-import xml.etree.ElementTree
+import xml.etree.ElementTree as ET
+
+# Variables
+###########
+LCDText = [" Program Starting..", "", "","reading config..."]
 
 # Parse the config file
-conf = xml.etree.ElementTree.parse('configuration.xml').getroot()
+# Modules Enable
+conf = ET.parse("configuration.xml").getroot()
+LCDEn = (conf.findall(".//module[@name='20x4LCD']"))[0].get("enable")
+SMSEn = (conf.findall(".//module[@name='SMS']"))[0].get("enable")
+SoundEn = (conf.findall(".//module[@name='Sound']"))[0].get("enable")
+WeatherEn = (conf.findall(".//module[@name='Weather']"))[0].get("enable")
+
+# Modules Data
+SMSNumbers = conf.findall(".//number[@name='num']")
+
+# Setup
+#######
+if (LCDEn == 'true'):
+	from modules.disp import disp
+	displayLoading("LCD initialized...")
+
+# Functions
+###########
+
+# Manages the 20x4LCD during loadingof the program
+def displayLoading(newline):
+	if (LCDEn == 'true'):
+		LCDText[1] = LCDText[2]
+		LCDText[2] = LCDText[3]
+		LCDText[3] = newline
+		disp.display(LCDText[0], LCDText[1], LCDText[2], LCDText[3])
--- modules/disp/disp.py
+++ modules/disp/disp.py
@@ -5,19 +5,25 @@ from time import *
 # lcd start
 lcd = lcddriver.lcd()
 
-# now we can display some characters (text, line)
+# Init the LCD driver
 def start():
     global lcd
     lcd = lcddriver.lcd()
 
+# Clear the LCD screen
 def clear():    
     lcd.lcd_clear()
 
+# Replaces screen with message
 def display(line1,line2,line3,line4):
     lcd.lcd_display_string(line1, 1)
     lcd.lcd_display_string(line2, 2)
     lcd.lcd_display_string(line3, 3)
     lcd.lcd_display_string(line4, 4)
+
+# Replaces line with message
+def display(line, linenumber):
+    lcd.lcd_display_string(line, linenumber)
 
 '''
 lcd.lcd_display_string("    Test Counter     ", 1)
