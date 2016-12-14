64cf7151a9a7336c9e83814d9799683f9f4b24d0
Timothy Lock <timothy@timothylock.ca>
2016-12-14 17:31:59 -0500

    Sync with Pi

---
+++ assets/js/weather/weatherupdate.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {  
+  getWeather(); //Get the initial weather.
+  setInterval(getWeather, 600000); //Update the weather every 10 minutes.
+});
+
+function getWeather() {
+  $.simpleWeather({
+    location: 'Toronto, ON',
+    unit: 'c',
+    success: function(weather) {
+      
+      //html = '<img src="'+weather.image+'">';
+      html = '<h2 style="font-size: 50px; text-align: left; margin-left: 20px; margin-top: 100px;">'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<h2 style="font-size: 20px; text-align: left; margin-left: 20px; line-height: 35%">'+weather.currently+'</h2>';
+      console.log(weather.low);
+      html += '<h2 style="font-size: 20px; text-align: left; margin-left: 20px; line-height: 160%"> Low: '+weather.low+' High: '+weather.high+'</h2>';
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+}
---
+++ old/OLD/Server/modules/sound/sound[1].py
@@ -0,0 +1,5 @@
+import os
+
+def play(file):
+    os.system('omxplayer -o local ' + file)
+
