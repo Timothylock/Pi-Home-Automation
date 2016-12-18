b5ad417f5b8de7244658a8e701394ac9b4f2a91b
Timothy Lock <lock.k.timothy@gmail.com>
2016-12-18 16:49:37 -0500

    old code moved to legacy branch

--- old/modules/sound/sound.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def play(file):
-    os.system('omxplayer -o local ' + file)
-
-print("Sound module initialized")
--- old/modules/speech/tts[1].py
+++
@@ -1,6 +0,0 @@
-import os
-
-def speak(line):
-    # Say the requesting thing
-    os.system("bash tts.sh " + line)
-
