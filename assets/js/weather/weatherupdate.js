88a887597a8f4baa9fed388305ec3fdd8e8ca64d
Timothy Lock <lock.k.timothy@gmail.com>
2017-01-17 20:09:14 -0500

    general optimizations + bug fixes

--- assets/js/weather/weatherupdate.js
+++ assets/js/weather/weatherupdate.js
@@ -1,8 +1,3 @@
-$(document).ready(function() {  
-  getWeather(); //Get the initial weather.
-  setInterval(getWeather, 600000); //Update the weather every 10 minutes.
-});
-
 function getWeather() {
   $.simpleWeather({
     location: 'Toronto, ON',
@@ -12,12 +7,16 @@ function getWeather() {
       //html = '<img src="'+weather.image+'">';
       html = '<h2 style="font-size: 4vw; text-align: left; margin-left: 20px; margin-top: 100px;">'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
       html += '<h2 style="font-size: 2vw; text-align: left; margin-left: 20px; line-height: 35%">'+weather.currently+'</h2>';
-      console.log(weather.low);
       html += '<h2 style="font-size: 2vw; text-align: left; margin-left: 20px; line-height: 160%"> Low: '+weather.low+' High: '+weather.high+'</h2>';
       $("#weather").html(html);
     },
     error: function(error) {
-      $("#weather").html('<p>'+error+'</p>');
+      html = '<h2 style="font-size: 4vw; text-align: left; margin-left: 20px; margin-top: 100px;">N/A</h2>';
+      html += '<h2 style="font-size: 2vw; text-align: left; margin-left: 20px; line-height: 35%">Cannot fetch weather</h2>';
+      html += '<h2 style="font-size: 2vw; text-align: left; margin-left: 20px; line-height: 160%"> Low: N/A High: N/A</h2>';
+      $("#weather").html(html);
     }
   });
+
+  setInterval(getWeather, 600000); //Update the weather every 10 minutes.
 }
