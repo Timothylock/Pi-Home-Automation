42b69fe2b06440385d85d155c0f67904f52c8df9
Timothy Lock <lock.k.timothy@gmail.com>
2017-05-22 19:18:00 -0400

    Fix font response and added live clock

---
+++ beta/assets/js/scripts.js
@@ -0,0 +1,20 @@
+function updateClock() {
+    var now = new Date(), // current date
+        months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ampm;
+
+    // a cleaner way than string concatenation
+    var date = [months[now.getMonth()], now.getDate()].join(' ');
+
+    // Update date and time elements
+    $('#date').html(date);
+    $('#time').html(strTime);
+}
+
+setInterval(updateClock, 1000);
