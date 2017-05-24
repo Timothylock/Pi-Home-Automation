3b89a2a86924474528302aefff82ed345afc08ae
Timothy Lock <lock.k.timothy@gmail.com>
2017-05-24 00:57:07 -0400

    Bug fix

--- beta/assets/js/scripts.js
+++ beta/assets/js/scripts.js
@@ -57,7 +57,7 @@ function togglehistoryview() {
             for(var i = response.length - 1; i >= 0; i--){
                 var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
                 d.setUTCSeconds(response[i].substring(0,response[i].length - 4)/1000);
-                insert += "<li class='list-group-item' onclick='showPicture(response[i])'>" + d + "</li>";
+                insert += "<li class='list-group-item' onclick='showPicture(" + response[i] + ")'>" + d + "</li>";
             }
 
             insert += "</ul>";
